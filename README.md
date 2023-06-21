c71f4add2f77db37318c716a79cd2dfbe78c537a
Uwe Schwarz <uwe@iq42.de>
2023-06-21 15:11:50 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,99 @@
+# rte
+
+remote timed executions
+
+## motivation
+
+I manage devices for my family and therefore need to backup the data. There are
+a few obstacles:
+- I don't want my own user on those devices, I just don't want to be
+  responsible if anything breaks.
+- I still want to provide some comfort by backing up essential data.
+- Immediate transfer between devices is managed elsewhere (iCloud in my case).
+- Backup should just work and I want to get the errors without any interaction
+  from my family.
+
+That's the reasons I wrote this script, basically all of this can achieved via
+other means. The first version I build was written around [rsync time backup](https://github.com/laurent22/rsync-time-backup/),
+but I found out that this isn't supported by [Hetzner Storage Boxes](https://www.hetzner.com/storage/storage-box).
+Therefore I switched the backup solution and wanted something even more simple,
+so it's just now a pull wahtever is there and execute that.
+
+## config server and connectivity
+
+Config is provided via ssh from somewhere, this should most likely be running
+24/7.
+
+The config for the whole system is kept on this system and must be
+accessible from the clients. I'm using [Tailscale](https://tailscale.com) with
+[SSH integration](https://tailscale.com/tailscale-ssh/) to offer a fairly
+simple way to just connect via ssh. SSH access must be possible.
+
+My setup:
+```
+/backup → symlink to a mountpoint with enough space
+/backup/config → directory with all the configs and logs
+/backup/config/logs/$hostname → output of backups, including stderr
+/backup/config/$hostname/run → script which gets executed
+```
+
+## client
+
+Every client needs bash, git, ssh and rsync.
+
+Download `rte.sh` and run `backup.sh nit <url>`. url is the
+config-directory-endpoint and must be accessible via rsync:
+
+> user@hostname:/backup/config
+
+The script uses two directories:
+- ~/.config/rte
+- ~/.local/share/rte
+
+(It tries to respect `XDG_CONFIG_HOME` and `XDG_DATA_HOME`.)
+
+On Linux a `systemd` timer gets created. On macOS a application gets created
+and a launchd timer created. This application needs most likely full disk
+access.
+
+## config
+
+Everything in /backup/config (see previous chapter) is just simple files, there
+is just a single config file. This config gets redownloaded on every run and a
+change of location is possible, just keep the old one around long enough for
+all clients to pick up the new one.
+
+Config file:
+
+/backup/config/defaults (shell like variables)
+```
+# config url changes get saved on the next run, keep the old place available for some time
+config=user@hostname:/backup/config
+
+# run every x seconds
+interval=10800
+
+# run on battery power (macOS only for now)
+battery=no
+```
+
+Then there is an additional directory:
+
+> /backup/config/hostname
+
+you can place anything inside, it gets downloaded (on every run) to the client.
+The file `run` inside this directory gets executed. The current directory is
+set to the place of `run`, there is also a `$exec_dir` with the temp-directory
+and `$log` where log output can be redirected. Stdout and stderr gets also
+logged.
+
+## log files
+
+log files (simple redirect from stdout/stderr) will get uploaded to
+`$config/logs/$hostname/$(date -u +%F-%H.%M.%S).log`. Updates will have a
+update.log extenstion.
+
+## caveats
+
+I use this only with tailscale and therefore don't need to handle ssh-keys. It
+should just work, but no guarantee.
---
+++ rte.sh
@@ -0,0 +1,219 @@
+#!/usr/bin/env bash
+
+# set xdg_variables
+APP="rte"
+
+CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/$APP"
+DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/$APP"
+
+if [ "$1" == "fda" ]; then
+  # just exit, hopefully agent will get populated in full disk access setting
+  exit 0
+fi
+
+if [ "$1" == "init" ]; then
+  # handling of client initialization
+  if [ "$2" == "--force" ]; then
+    # force remove old installs
+    rm -fr "$CONFIG_DIR" "$DATA_DIR"
+    shift
+  fi
+  # check if CONFIiG_DIR already exists
+  if [ -d "$CONFIG_DIR" ]; then
+    echo "$CONFIG_DIR already exists, start with"
+    echo "  $0 init --force <url>"
+    echo "to reconfigure."
+    exit 1
+  fi
+
+  mkdir -p "$CONFIG_DIR" "$DATA_DIR"
+  chmod 700 "$CONFIG_DIR" "$DATA_DIR"
+
+  echo "downloading defaults"
+  rsync --quiet --timeout=5 "$2/defaults" "$CONFIG_DIR"
+  if [ $? -ne 0 -a ! -f "$CONFIG_DIR/defaults" ]; then
+    echo "can't reach $2 (or no config present), removing $CONFIG_DIR and giving up"
+    rm -fr "$CONFIG_DIR" "$DATA_DIR"
+    exit 2
+  fi
+
+  # initial git pull
+  echo "getting the newest version from github and place it in $DATA_DIR"
+  cd "$DATA_DIR"
+  git clone https://github.com/uwe-schwarz/rte.git 2>&1 | sed 's/^/  /'
+
+  # got main config files, get interval and create timers
+  source "$CONFIG_DIR/defaults"
+
+  echo "creating timers with interval=$interval"
+  case $(uname) in
+    Linux)
+      echo "operating system is Linux, using a systemd user timer"
+      echo "this creates a timer in ~/.config/systemd/user, maybe this is the wrong directory"
+      mkdir -p ~/.config/systemd/user
+      cat > ~/.config/systemd/user/rte.service << EOF
+[Unit]
+Description=rte
+After=network.target
+ConditionPathExists=$CONFIG_DIR
+
+[Service]
+Type=oneshot
+ExecStart=$DATA_DIR/rte/rte.sh
+
+[Install]
+WantedBy=default.target
+EOF
+      cat > ~/.config/systemd/user/rte.timer << EOF
+[Unit]
+Description=Run "rte" every $interval seconds
+
+[Timer]
+OnBootSec=30min
+OnUnitActiveSec=$interval
+
+[Install]
+WantedBy=timers.target
+EOF
+      systemctl --user daemon-reload
+      systemctl --user enable --now "rte.timer"
+      echo "systemd timer enabled."
+      ;;
+    Darwin)
+      echo "operating system is macOS, using a fake application and launchd timer"
+      mkdir -p ~/Applications
+      osacompile -o ~/Applications/rte.app -e 'do shell script "'"$DATA_DIR"'/rte/rte.sh"'
+      open -g -j -a ~/Applications/rte.app --args fda
+      echo "give full disk access or the needed rights to ~/Applications/rte.app/Contents/MacOS/applet."
+      echo "it's probably best to give fda rights, otherwise the errors will get fuzzy do debug later."
+      echo "if there is no applet in the list, add ~/Applications/rte.app/Contents/MacOS/applet"
+      read
+      echo "making LaunchAgent"
+      mkdir -p ~/Library/LaunchAgents
+      cat > ~/Library/LaunchAgents/com.github.uwe-schwarz.rte.plist << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+  "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>com.github.uwe-schwarz.rte</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>open</string>
+      <string>-g</string>
+      <string>-j</string>
+      <string>-a</string>
+      <string>$HOME/Applications/rte.app</string>
+    </array>
+    <key>StartInterval</key> 
+    <integer>$interval</integer>
+</dict>
+</plist>
+EOF
+      launchctl unload ~/Library/LaunchAgents/com.github.uwe-schwarz.rte.plist >/dev/null 2>/dev/null
+      launchctl load ~/Library/LaunchAgents/com.github.uwe-schwarz.rte.plist
+
+      echo "don't remove ~/Applications/rte.app"
+      ;;
+    *)
+      echo "unknown system, please create timer yourself: start $DATA_DIR/rte/rte.sh every $interval seconds."
+      exit 3
+      ;;
+  esac
+
+  exit 0
+fi
+
+# are there other arguments?
+if [ $# -ne 0 ]; then
+  echo "start like this for first invocation:"
+  echo "$0 init [--force] <url>"
+fi
+
+# normal operation
+
+# read defaults
+cd "$CONFIG_DIR"
+source "$CONFIG_DIR/defaults"
+
+# check for battery power
+if [ "$battery" == "no" ]; then
+  pmset -g ps | grep -q "AC" || exit 0
+fi
+
+# hostname, only till first "."
+h="$(hostname | cut -d. -f1)"
+
+# start time
+t="$(date -u +%F-%H.%M.%S)"
+
+# redownload config
+rsync --quiet --timeout=5 "$config/defaults" "$CONFIG_DIR"
+if [[ $? -ne 0 ]]; then
+  # config host not reachable, quit quietly
+  exit 0
+fi
+
+# variables again
+source "$CONFIG_DIR/defaults"
+
+# TODO interval changed handling
+
+# local logfile and basic redirection
+logfile="$(mktemp)"
+exec >$logfile 2>&1 </dev/null
+
+# make directory for logfile upload
+ssh ${config/:*} "mkdir -p \"${config/*:}/logs/$h\""
+
+# first thing update and restart if needed
+cd "$DATA_DIR/rte"
+git remote update >/dev/null 2>/dev/null
+if git status -uno | grep -q "Your branch is behind"; then
+  # there are updates, pull and restart
+  git status
+  git pull
+  if [[ -f "$logfile" ]]; then 
+    rsync --quiet --timeout=10 "$logfile" "$config/logs/$h/$t.update.log"
+    rm -f "$logfile"
+  fi
+  # restart after upgrade
+  exec $0
+fi
+
+# no more updates, all base config is ok, redirects done. Starting execution
+
+# create temp file and download hostname-folder
+exec_dir="$(mktemp -d)"
+
+rsync -r "$config/$h" "$exec_dir"
+chmod +x "$exec_dir/$h/run"
+
+# run hostname/run command and keep time
+secstart="$(date +%s)"
+echo "starting: $h/run"
+
+# create $log as file for logging
+export log="$(mktemp)"
+# run
+"$exec_dir/$h/run"
+result=$?
+cat "$log"
+
+# remove temp files and directory
+rm -f "$log"
+rm -fr "$exec_dir"
+
+# conclude
+secstop="$(date +%s)"
+echo "completed: $h/run in $(($secstop-$secstart)) seconds, exit code $result"
+
+# TODO notify
+
+# upload logs and exit
+rsync --quiet --timeout=10 "$logfile" "$config/logs/$h/$t.log"
+
+# remove log
+rm -f "$logfile"
+exit 0
